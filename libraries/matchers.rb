ad056a2461b0a8ae445fdf50127fd36893b4b186
Hemal <h.n.varambhia@gmail.com>
2015-02-19 11:20:27 +0000

    Readable matcher factory method for the chefspec port listening matcher

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -2,4 +2,8 @@ if defined?(ChefSpec)
   def set_server_names_to(*server_names)
     ChefSpec::Matchers::NginxServerNameMatcher.new(chef_run, server_names)
   end
+
+  def listen_to_port(port)
+    ChefSpec::Matchers::NginxPortListenedToMatcher.new(chef_run, port)
+  end
 end
