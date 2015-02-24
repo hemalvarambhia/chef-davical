dbe272da185c171129a56019c3c352f491c8b13a
Hemal <h.n.varambhia@gmail.com>
2015-02-24 11:52:16 +0000

    Updated the README, updated the version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'h.n.varambhia@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures davical'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 supports "ubuntu"
 
