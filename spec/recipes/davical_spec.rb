8faae6479bc6605b8000b2914912de0e4c7f020a
Hemal N Varambhia <h.n.varambhia@gmail.com>
2015-08-23 16:28:24 +0100

    Wrote up the chefspec tests for installing davical on Ubuntu and CentOS

--- spec/recipes/davical_spec.rb
+++ spec/recipes/davical_spec.rb
@@ -1,23 +1,34 @@
 require_relative '../spec_helper.rb'
 
 describe "chef-davical::davical" do
-  context "Ubuntu" do
+  context "Installing on Ubuntu" do
     
     let(:chef_run) { ChefSpec::SoloRunner.new(platform: "ubuntu", version: "14.04") do |node|
 
     end.converge(described_recipe)}
   
 
-    it "installs davical"
+    it "installs davical deb package" do
+      expect(chef_run).to install_package "davical"
+    end
   end
 
-  context "CentOS" do
+  context "Installing on CentOS" do
     let(:chef_run) { ChefSpec::SoloRunner.new(platform: "centos", version: "7.0") do |node|
                        
-                     end.converge(describe_recipe) }
+                     end.converge(described_recipe) }
 
-    it "install git"
-    it "clones davical and awl source codes"
-    it "creates the /etc/davical config directory"
+    it "installs git" do
+      expect(chef_run).to install_package "git"
+    end
+    
+    it "clones davical and awl source codes" do
+      expect(chef_run).to sync_git("/usr/share/davical").with(repository: "https://gitlab.com/davical-project/davical.git")
+      expect(chef_run).to sync_git("/usr/share/awl").with(repository: "https://gitlab.com/davical-project/awl.git")
+    end
+    
+    it "creates the /etc/davical config directory" do
+      expect(chef_run).to create_directory "/etc/davical"
+    end
   end
 end
