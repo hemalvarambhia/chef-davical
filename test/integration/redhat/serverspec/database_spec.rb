772a9d155a752344a781331104a87d79c0d6a1d6
Hemal <h.n.varambhia@gmail.com>
2015-08-25 15:12:33 +0100

    Test that the DAViCal database is created

--- test/integration/redhat/serverspec/database_spec.rb
+++ test/integration/redhat/serverspec/database_spec.rb
@@ -9,3 +9,9 @@ describe "postgresql server" do
     it { should be_running }
   end
 end
+
+describe command("sudo su - postgres -c \"psql -c 'select datname from pg_database;'\"") do
+    its(:stdout) { should match /davical/ }
+
+    its(:exit_status) { should eq 0 }
+end
